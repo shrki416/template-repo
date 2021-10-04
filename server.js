edcf7d7f1ff77d78c66660a10dfbc6b51150e6a9
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-10-03 20:34:50 -0400

    Template Repo

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(PORT, () => console.log(`Server Started on port: ${PORT}`));
